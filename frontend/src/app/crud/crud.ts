de84c5d268c219ebae694cac9392beb154ca81c9
gadyelbertini <gadyelbertini@gmail.com>
2026-05-05 22:09:53 -0300

    Formulário

--- frontend/src/app/crud/crud.ts
+++ frontend/src/app/crud/crud.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-crud',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './crud.html',
   styleUrl: './crud.css',
 })
-export class Crud {}
+export class Crud {
+
+  // Visibilidade dos botões
+  btnCadastrar:boolean = true;
+}
