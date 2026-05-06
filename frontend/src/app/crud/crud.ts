7711f4f5b2194511200796adfea78c8ed54bd72f
gadyelbertini <gadyelbertini@gmail.com>
2026-05-05 22:32:31 -0300

    Seleção

--- frontend/src/app/crud/crud.ts
+++ frontend/src/app/crud/crud.ts
@@ -5,10 +5,12 @@ import { MatInputModule } from '@angular/material/input';
 import { ApiPessoas } from '../service/api-pessoas';
 import { Pessoa } from '../module/Pessoa';
 import { MatTableModule } from '@angular/material/table';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-crud',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule, ReactiveFormsModule],
   templateUrl: './crud.html',
   styleUrl: './crud.css',
 })
@@ -23,6 +25,13 @@ export class Crud {
   // Vetor para armazenar as pessoas
   vetor:Pessoa[] = [];
 
+  // Objeto - FormulÃ¡rio Reativo
+  formularioPessoa = new FormGroup({
+    id: new FormControl(),
+    nome: new FormControl(),
+    cidade: new FormControl()
+  });
+
   constructor(private servico:ApiPessoas){}
 
   // ngOnInit - Executa este mÃ©todo apÃ³s o componente ser montado
@@ -33,5 +42,30 @@ export class Crud {
   // MÃ©todo para selecionar todas as pessoas da API
   listar():void{
     this.servico.listar().subscribe(pessoas => this.vetor = pessoas);
+  }
+
+  // MÃ©todo para cadastrar pessoas
+  cadastrar():void{
+    // Criar um novo objeto
+    let obj = {...this.formularioPessoa.value}; // Copia todas as caracterÃ­sticas do nosso formulÃ¡rio reativo (via spred operator).
+    delete obj.id; // Remove o id, para que nossa API receba um objeto contendo apenas o nome e a cidade.
+
+    // Realizar a requisiÃ§Ã£o de cadastro (POST) e atualizar o vetor
+    this.servico.cadastrar(obj).subscribe(pessoa => this.vetor = [...this.vetor, pessoa]);
+
+    // Limpar o formulÃ¡rio
+    this.formularioPessoa.reset();
+  }
+  
+  // MÃ©todo para selecionar uma pessoa especÃ­fica
+  selecionarPessoa(id:string):void{
+    this.servico.selecionarPessoa(id).subscribe(pessoa => {
+
+      // Disponibiliza um objeto com as caracterÃ­sticas: id, nome e cidade para o nosso formulÃ¡rio reativo
+      this.formularioPessoa.patchValue(pessoa);
+
+      // Visibilidade dos botÃµes
+      this.btnCadastrar = false;
+    });
   }
 }
