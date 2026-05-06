1ba88ccf5c6e1bcc414725aea21988bda87fa585
gadyelbertini <gadyelbertini@gmail.com>
2026-05-05 22:29:11 -0300

    Cadastro

--- frontend/src/app/crud/crud.ts
+++ frontend/src/app/crud/crud.ts
@@ -3,10 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ApiPessoas } from '../service/api-pessoas';
+import { Pessoa } from '../module/Pessoa';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-crud',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule],
   templateUrl: './crud.html',
   styleUrl: './crud.css',
 })
@@ -15,5 +17,21 @@ export class Crud {
   // Visibilidade dos botões
   btnCadastrar:boolean = true;
 
+  // Colunas da tabela
+  colunas:String[] = ['id', 'nome', 'cidade', 'selecionar'];
+
+  // Vetor para armazenar as pessoas
+  vetor:Pessoa[] = [];
+
   constructor(private servico:ApiPessoas){}
+
+  // ngOnInit - Executa este mÃ©todo apÃ³s o componente ser montado
+  ngOnInit():void{
+    this.listar();
+  }
+
+  // MÃ©todo para selecionar todas as pessoas da API
+  listar():void{
+    this.servico.listar().subscribe(pessoas => this.vetor = pessoas);
+  }
 }
